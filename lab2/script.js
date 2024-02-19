bc26de51692213e81a57f23edc03db86e03c690a
anavasynth <vadimgonchariuk@gmail.com>
2024-02-19 12:22:21 +0200

    lab 2 ready to pass!

--- lab2/script.js
+++ lab2/script.js
@@ -1,4 +1,5 @@
 //Завдання 1
+
 function MinAndMax(numbers){
     let min = numbers[0];
     let max = numbers[0];
@@ -25,7 +26,58 @@ function ShowMinAndMax(){
     result.innerHTML = "Мінімальний: " + minAndMax.min + " Максимальний: " + minAndMax.max;
 }
 
+function CompareCars() {
+    let brand1 = document.getElementById("brand1").value;
+    let brand2 = document.getElementById("brand2").value;
+
+    let color1 = document.getElementById("color1").value;
+    let color2 = document.getElementById("color2").value;
+
+    let year1 = document.getElementById("year1").value;
+    let year2 = document.getElementById("year2").value;
+
+    const car1 = {brand: brand1, color: color1, year: year1};
+    const car2 = {brand: brand2, color: color2, year: year2};
+
+    let allInfo = '';
+
+    if (car1.brand.toLowerCase() == car2.brand.toLowerCase()){
+        allInfo += "Машина 1 і Машина 2 однакової марки - " + car1.brand + ". ";
+    }
+    else {
+        allInfo += "Машини різних марок. ";
+    }
+
+    if(car1.color.toLowerCase() == car2.color.toLowerCase()){
+        allInfo += "Машини однакового кольору - " + car1.color + ". ";
+    }
+    else {
+        allInfo += "Машини різного кольору. ";
+    }
+
+    if (car1.year == '' && car2.year == ''){
+            allInfo += "Рік не вказано!";
+        }
+    else if((car1.year < 2024 && car1.year > 1941) && (car2.year < 2024 && car2.year > 1941)){
+        if (car1.year == car2.year){
+            allInfo += "Машини одного року випуску. "
+        }
+        else if(car1.year > car2.year){
+            allInfo += "Машина 2 випущена раніше.";
+        }
+        else if(car1.year < car2.year){
+            allInfo += "Машина 1 випущена раніше.";
+        }
+    }
+    else{
+        allInfo += "Рік не коректний! (Вкажіть від 1941 до 2024)";
+        }
+
+    alert(allInfo);
+}
+
 //Завдання 2
+
 function IsInRange(){
     let numInRange = document.getElementById("numberInRange").value;
     let firstNum = document.getElementById("firstNum").value;
@@ -44,15 +96,12 @@ function IsInRange(){
 
 function ChangeBool() {
     let change = prompt("Enter 'true' or 'false'")
-    if(change === 'true' || change === 'false'){
-
-        change = change === 'true';
-
+    if(change || !!change){
         if (change){
-            alert("True тепер " + !change);
+            alert("True тепер " + !!change);
         }
         else{
-            alert("False тепер " + !change);
+            alert("False тепер " + !!change);
         }
     }
     else{
